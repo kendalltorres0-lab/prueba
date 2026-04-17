59dfa178888a02a69f3855cd28cd63a1de25c58a
kendalltorres0-lab <kendalltorres0@gmail.com>
2026-04-16 22:33:38 -0600

    quinta prueba

---
+++ index.js
@@ -0,0 +1,15 @@
+let nombre = document.getElementById("nombre")
+let apellido = document.getElementById("apellido")
+let edad = document.getElementById("edad")
+let button = document.getElementById("button")
+
+button.addEventListener("click",guardar)
+function guardar(){
+alert(nombre.value +"  " + apellido.value + "   " + edad.value)
+
+
+
+
+
+
+}
